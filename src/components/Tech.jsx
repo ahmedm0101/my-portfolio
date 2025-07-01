0a06f4184d1467baf48f5912497990c987dcbf99
ahmed mahmoud <ahmad.mahmoud1472003@gmail.com>
2025-07-01 20:34:40 +0300

    work

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { technologies } from "../constants";
 import { BallCanvas } from "./canvas";
+import styles from "../styles";
+
 
 const Tech = () => {
   return (
-    <div className="flex justify-center flex-wrap flex-row gap-10">
+    <div className={`flex justify-center flex-wrap flex-row gap-10 ${styles.paddingX}`}>
       {technologies.map((tech) => (
         <div className="w-28 h-28" key={tech.name}><BallCanvas icon={tech.icon}/></div>
       ))}
