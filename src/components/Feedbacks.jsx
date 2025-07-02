4e1c2f86f44a50e1ee401e657e111509761c347e
ahmed mahmoud <ahmad.mahmoud1472003@gmail.com>
2025-07-02 11:29:31 +0300

    'testimonial'

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import React from "react";
+import styles from "../styles";
+import SectionWrapper from "../hoc/SectionWrapper";
+import { testimonials } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
+const FeedbackCard = ({
+  index,
+  testimonial,
+  name,
+  company,
+  image,
+  designation,
+}) => (
+  <motion.div
+    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    className="bg-black-200 rounded-3xl p-10 xs:w-[320px] w-full"
+  >
+    <p className="text-white text-[48px] font-black">"</p>
+    <div className="mt-1 text-white">
+      <p className="tracking-wider text-[18px]">{testimonial}</p>
+      <div className="mt-7 flex justify-between items-center gap-1">
+        <div className="flex flex-1 flex-col">
+          <p className="font-medium text-[16px]">
+            <span className="blue-text-gradient">@</span> {name}
+          </p>
+          <p className="mt-1 text-secondary text-[12px]">
+            {designation} of {company}
+          </p>
+        </div>
+          <img className="rounded-full w-10 h-10 object-cover" src={image} alt={`feedback-by-${name}`} />
+      </div>
+    </div>
+  </motion.div>
+);
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
-  )
-}
+    <div className="mt-12 bg-black-100 rounded-[20px]">
+      <div
+        className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}
+      >
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>What others say</p>
+          <h2 className={styles.sectionHeadText}>Testtimonials.</h2>
+        </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Feedbacks
+export default SectionWrapper(Feedbacks, "");
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { technologies } from "../constants";
 import { BallCanvas } from "./canvas";
 import styles from "../styles";
-
+import SectionWrapper from "../hoc/SectionWrapper";
 
 const Tech = () => {
   return (
-    <div className={`flex justify-center flex-wrap flex-row gap-10 ${styles.paddingX}`}>
+    <div className={`flex justify-center flex-wrap flex-row gap-10`}>
       {technologies.map((tech) => (
-        <div className="w-28 h-28" key={tech.name}><BallCanvas icon={tech.icon}/></div>
+        <div className="w-28 h-28" key={tech.name}>
+          <BallCanvas icon={tech.icon} />
+        </div>
       ))}
     </div>
   );
 };
 
-export default Tech;
+export default SectionWrapper(Tech, "");
